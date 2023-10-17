86b6e46232c015f4dac1cb70bf70f024445f8c02
ivan <imtambovtcev@gmail.com>
2023-10-17 08:42:30 +0000

    + load test

--- tests/test_roadmap.py
+++ tests/test_roadmap.py
@@ -4,7 +4,7 @@ from rkiskaupas_datathon import RoadMap
 class TestRoadMap:
     @classmethod
     def setup_class(cls):
-        cls.roadmap = RoadMap()
+        cls.HM = RoadMap.load('tests/HM.json')
 
-    def test_dummy(self):
-        pass
+    def test_load(self):
+        assert len(self.HM.nodes()) == 200
